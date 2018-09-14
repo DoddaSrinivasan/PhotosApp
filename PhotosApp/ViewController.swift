0655a0ecd69476ed8d339601710c1894e0ffb3d6
Srinivas Dodda <srinivasdodda@Srinivasdodda.local>
2018-09-14 14:22:34 +0200

    New xCode project

---
+++ PhotosApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotosApp
+//
+//  Created by Srinivas Dodda on 14/09/18.
+//  Copyright © 2018 Srinivas Dodda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
